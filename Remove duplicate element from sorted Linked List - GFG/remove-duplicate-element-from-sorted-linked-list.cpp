432291aee2657813b3b19a21b4e8fcbcbc125fff
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2022-06-10 11:58:04 +0530

    Added solution - LeetHub

---
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1/?page=1&difficulty[]=0&category[]=Linked%20List&sortBy=submissions#to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ 
+Node *temp=head;
+ 
+while(temp->next!=NULL)
+ {
+      if(temp->data == temp->next->data){
+          temp->next = temp->next->next;
+      }
+      else{
+          temp=temp->next;
+      }
+     
+ }
+ 
+ return head;
+ 
+}
